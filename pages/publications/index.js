1e36c821ef8099dc38734fb4a0ef75b653d20341
Rikke Saust <rlsaust@gmail.com>
2023-11-27 22:30:59 +0100

    simple nextJS setup

---
+++ pages/publications/index.js
@@ -0,0 +1,11 @@
+export default function PublicationsPage () {
+    return (
+        <>
+        <h1>List of Publications</h1>
+        <ul>
+            <li>List item</li>
+        </ul>
+
+        </>
+    );
+};
