b0f391c8a2c4d3945d352ca22318b52bda050634
Rikke Saust <rlsaust@gmail.com>
2023-12-04 13:50:32 +0100

    form to enter new profile content, with templates/suggestions for collaborators from top 5 co- authors

--- components/PapersField/PapersField.js
+++ components/PapersField/PapersField.js
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import Link from "next/link";
 
 
-export default  function PapersField ({userName}) {
+export default  function PapersField ({userName,getPapers}) {
     const { data:papers, isLoading, error } = useSWR(`/api/${userName}/papers`)
 
     if (isLoading) return (
@@ -17,6 +17,8 @@ export default  function PapersField ({userName}) {
         <h4>Error! {error.message}</h4>
         </> 
     )
+    getPapers(papers)
+
     return (
     <>
         <h2 className="papersFieldTitle" >Papers</h2>
--- pages/[userName]/index.js
+++ pages/[userName]/index.js
@@ -4,20 +4,34 @@ import LoginButton from "../../components/LoginButton/LoginButton";
 import PapersField from "../../components/PapersField/PapersField";
 import UserNavBar from "../../components/UserNavBar/UserNavBar";
 import Link from "next/link";
+import ProfileContentForm from "../../components/ProfileContentForm/ProfileContentForm";
+import { useEffect, useState } from "react";
 
 export default function MainProfilePage() {
+    const [papers,setPapers] = useState()
     const {data:session} = useSession();
     const router = useRouter()
     const currentPageOwner = router.query.userName
     
+
+
+
+
+    function getPapers(papersFromComponent) {
+        setPapers(papersFromComponent)
+    }
+
     return (
         <> {
             session?.user.name===currentPageOwner ? <h1> Your main profile page </h1> : <h1>Main profile page for {currentPageOwner}</h1> 
         }
             <UserNavBar userName={currentPageOwner}/>
-            <PapersField userName={currentPageOwner}/>
+            {session?.user.name===currentPageOwner &&
+            <ProfileContentForm userName={currentPageOwner} papers={papers}/> }
+
+            <PapersField getPapers={getPapers} userName={currentPageOwner}/>
             <LoginButton/>
-            <Link href={'/'}>go to homepage</Link>
+            <Link href={'/'}>Go to homepage</Link>
         </>
     )
     
