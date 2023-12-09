9a4a593cea743c81eedf0083d6bf55341573c109
Rikke Saust <rlsaust@gmail.com>
2023-12-09 19:28:37 +0100

    sorting function, for oldest/newest or most fav highlight

--- components/Highlights/Highlights.js
+++ components/Highlights/Highlights.js
@@ -1,18 +1,21 @@
 import HighlightForm from "../HighlightForm/HighlightForm"
 import ShowHighlight from "../ShowHighlight/ShowHighlight";
 import SaveButton from "../SaveButton/SaveButton";
-import FavButton from "../FavButton/FavButton";
-
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import useSWR from "swr";
-import { useState } from "react";
+import {  useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+import SortingSetting from "../SortingSetting/SortingSetting";
 
 export default function Highlights() {
     const [content, setContent] = useState();
     const [isContentSaved,setIsContentSaved] = useState(false);
     const [favInfo,setFavInfo] = useState([])
     const [isAddHighlight,setIsAddHighlight] = useState(false);
+    const [sortedBy,setSortedBy] = useLocalStorageState(('sortedBy', {
+            defaultValue: 'newest' }))
+    const [highlightsSorted,setHighlightsSorted] = useState([]);
     const {data:session} = useSession()
     const router = useRouter()
     const currentPageOwner = router.query.userName
@@ -41,8 +44,6 @@ export default function Highlights() {
         }
     }  
     async function handleUpdateHighlight(updatedFavChangeInfo) {
-        //--reset:
-        // favCountInfo['favCount']=0 
         const response = await fetch(`/api/${currentPageOwner}/highlights`, {
             method:'PATCH',
             headers:{
@@ -52,7 +53,6 @@ export default function Highlights() {
         },
         )
         if (response.ok) {
-            console.log('updated')
             mutate()
         }
     }
@@ -68,13 +68,10 @@ export default function Highlights() {
             if (infoForID.isFav===true) {
                 favChange = -1
             }
-            
-
             setFavInfo(
                 favInfo.map(info=> info.highlightID!==id ?
                 info:{...info,isFav:!info.isFav})
             )
-
         }
         if (!infoForID) {
             setFavInfo([...favInfo,{highlightID:id,isFav:true}])
@@ -83,12 +80,34 @@ export default function Highlights() {
         handleUpdateHighlight(favChangeInfo);
     }
 
+    //--sorting on first render:--
+    let firstHighligts
+    if (highlightsSorted.length === 0){
+        if (sortedBy === 'newest') firstHighligts = sortByNewest()
+        if (sortedBy === 'oldest') firstHighligts = sortByOldest()
+        if (sortedBy === 'mostFav') firstHighligts = sortByMostFav()
+    }
+    function sortByOldest() {
+        return highlights.sort((a,b)=> 
+        //get a number/integer for all the createdAt dates by removing non-digits(/\D/g ):
+        a.createdAt.replace(/\D/g, '') - b.createdAt.replace(/\D/g, ''))
+    }
+    function sortByNewest () {
+        return highlights.sort((a,b)=> 
+        b.createdAt.replace(/\D/g, '') - a.createdAt.replace(/\D/g, ''))
+    }
+    function sortByMostFav () {
+        return highlights.sort((a,b)=> 
+        b.favCount - a.favCount)
+    }
+    function handleSortingHighlights(sortSetting) {
+        setSortedBy(sortSetting)
+        firstHighligts = null
+        if (sortSetting==='oldest') setHighlightsSorted(sortByOldest())
+        if (sortSetting==='newest') setHighlightsSorted(sortByNewest())
+        if (sortSetting==='mostFav') setHighlightsSorted(sortByMostFav())
+    }
 
-    //---sort by created date/time---
-    const highlightsSorted = highlights.sort((a,b)=> 
-    //get a number/integer for all the createdAt dates by removing non-digits(/\D/g ):
-        b.createdAt.replace(/\D/g, '')-a.createdAt.replace(/\D/g, '')
-    )
 
     return (
         <>
@@ -100,21 +119,30 @@ export default function Highlights() {
             
         </>
         }
-            { content ?
+            { content &&
             <>
             <SaveButton isSaved={isContentSaved} onSave={handleAddHighlight} itemSaved={'highlight'}/> 
             <ShowHighlight content={content}/>
-            </>
-            :''} 
+            </>} 
             {highlights ? 
+            <>
+            <SortingSetting handleSortingHighlights={handleSortingHighlights} sortedBy={sortedBy}/>
+            {firstHighligts ? 
+                (firstHighligts?.map( highlight => 
+                    <section key={highlight._id} className="highlightContainer" >
+                    <ShowHighlight favInfo={favInfo} handleToggleFav={handleToggleFav} content={highlight}/> 
+                    </section>
+                    )
+                ):
                 (highlightsSorted?.map( highlight => 
                     <section key={highlight._id} className="highlightContainer" >
-                     {/* <FavButton  content={highlight} favInfo={favInfo} handleToggleFav={handleToggleFav} /> */}
-                    <ShowHighlight content={highlight} favInfo={favInfo} handleToggleFav={handleToggleFav}/> 
+                    <ShowHighlight favInfo={favInfo} handleToggleFav={handleToggleFav} content={highlight}/> 
                     </section>
                     )
                 )
-            :''} 
+            }
+            </>
+            :<p > No highlights added for this profile</p>}
 
         </>
         
