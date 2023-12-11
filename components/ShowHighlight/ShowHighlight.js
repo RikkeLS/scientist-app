c31bb3dc1d739f5e8d9d3a9997b179498b5df519
Rikke Saust <rlsaust@gmail.com>
2023-12-11 13:15:18 +0100

    hide delete button for visiting users and hide fav from currentProfileOwner

--- components/ShowHighlight/ShowHighlight.js
+++ components/ShowHighlight/ShowHighlight.js
@@ -1,13 +1,20 @@
 import Link from "next/link";
 import FavButton from "../FavButton/FavButton";
 import DeleteButton from "../DeleteButton/DeleteButton";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function ShowHighlight({content,favInfo,handleToggleFav,handleDeleteHighlight}) {
+    const {data:session} = useSession();
+    const router = useRouter();
+    const currentPageOwner = router.query.userName;
     return (
         <>
             <section className="highlight_titleContainer">
-            <DeleteButton handleDelete={handleDeleteHighlight} ID={content._id}/> 
+            {session?.user.name===currentPageOwner ? <DeleteButton handleDelete={handleDeleteHighlight} ID={content._id}/> :
             <FavButton  content={content} favInfo={favInfo} handleToggleFav={handleToggleFav} />
+            }
+            
                 <h3 className="highlight_title">{content.title}</h3>
                 <Link className="highlight_refLink" href={content.refLink} rel='noopener noreferrer' target='_blank'>{content.refText}  </Link>
             </section>
